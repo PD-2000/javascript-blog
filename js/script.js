a9d458c6cfff08db5e21b88c48454f1b9705ebf3
Pawel Domagala <domagalapawel11@gmail.com>
2022-12-10 23:31:30 +0100

    Change content of titleClickHandler

--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
 'use strict';
 
 const titleClickHandler = function(event){
+	const clickedElement = this;
 	console.log('Link was clicked!');
 
-	/* remove class 'active' from all article links  */
+	/* [DONE] remove class 'active' from all article links  */
 	const activeLinks = document.querySelectorAll('.titles a.active');
 
 	for(let activeLink of activeLinks){
 		activeLink.classList.remove('active');
 	}
 
-	/* add class 'active' to the clicked link */
+	/* [IN PROGRESS] add class 'active' to the clicked link */
+	console.log('clickedElement:', clickedElement);
 
-	/* remove class 'active' from all articles */
-	const activeArticles = document.querySelectorAll('a.active');
+	clickedElement.classList.add('active');
+
+	/* [DONE] remove class 'active' from all articles */
+	const activeArticles = document.querySelectorAll('.article a.active');
 
 	for(let activeArticle of activeArticles){
 		activeArticle.classList.remove('active');
@@ -24,8 +28,6 @@ const titleClickHandler = function(event){
 	/* find the correct article using the selector (value of 'href' attribute) */
 
 	/* add class 'active' to the correct article */
-
-	console.log(event);
 }
 
 const links = document.querySelectorAll('.titles a');
